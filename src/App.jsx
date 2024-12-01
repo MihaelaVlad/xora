5ce3dd6abaa949f8758a6bfdc92bbf70e7b83b9c
Mihaela Vlad <vlad.c.mihaela@gmail.com>
2024-12-01 20:11:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header></Header>
+      <Hero></Hero>
+    </main>
+  );
+};
+
+export default App;
